7f938d306b0de316df73e7243898d985a4b086bc
nykolayr_tomsk <nykolayr@gmail.com>
2023-08-05 20:13:21 +0700

    +

--- lib/ui/splash/sub/update/update_page.dart
+++ lib/ui/splash/sub/update/update_page.dart
@@ -4,12 +4,10 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:expansion/ui/battle/widgets/widgets.dart';
 import 'package:expansion/ui/splash/sub/update/bloc/update_bloc.dart';
 import 'package:expansion/ui/widgets/widgets.dart';
-import 'package:expansion/utils/colors.dart';
 import 'package:expansion/utils/value.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class UpdatePage extends StatelessWidget {
   const UpdatePage({Key? key}) : super(key: key);
